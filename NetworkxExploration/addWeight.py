c230ada530013c44536f5b50c4b352e11b5430b0
Tofull <loic.messal@orange.fr>
2017-03-15 00:56:37 +0100

    set weight to edge

--- NetworkxExploration/addWeight.py
+++ NetworkxExploration/addWeight.py
@@ -13,23 +13,45 @@ def read_json_file(filename):
 
 def selectEdge(graph, source, target):
     id = 0
-    for e in graph.edges_iter(data=True):
-        print(e[0], e[1])
-        if (source == e[0] and target == e[1]):
+    for u,v,d in G.edges_iter(data=True):
+        if (source == u and target == v):
             return id
         id += 1
 
+    #
+    #
+    # for e in graph.edges_iter(data=True):
+    #     print(e[0], e[1])
+    #     if (source == e[0] and target == e[1]):
+    #         return id
+    return id
+
 
 source = "53062784"
-target = "53062794"
+target = "4241681602"
 
 filename = 'JSONData.json'
 G = read_json_file(filename)
-# print(G.node[source])
-print(G.edges(data=True))
+# print(G.nodes(data=True))
+# print(G.edges(data=True))
+
+# print(G[int(source)][int(target)])
+e = selectEdge(G, source, target)
+print("source :", source, "target :", target)
+print("edge :", G.edges(data=True)[e])
+
+source = "4241681602"
+target = "53062785"
+e = selectEdge(G, source, target)
+print("source :", source, "target :", target)
+print("edge :", G.edges(data=True)[e])
+
+defaultWeight = 3.0
+G.add_weighted_edges_from([( G.edges(data=True)[e][0], G.edges(data=True)[e][1], defaultWeight)])
 
 
-# e = selectEdge(G, source, target)
+
+print("edge :", G.edges(data=True)[e])
 # print(e)
 # plt.plot([G.node[n]['lat']for n in G], [G.node[n]['lon'] for n in G], 'o', color='k')
 # plt.show()
