ddddcd75f02f351b3f406ba4431031b7a75c06aa
MILLET <rmillet8@sfr.fr>
2017-03-15 17:08:25 +0100

    Mise en ligne
    Mise en ligne de l'interface web basique

---
+++ Web src/js/menu.js
@@ -0,0 +1,7 @@
+function displaynigeria(){
+	map.setView(new L.LatLng(4.804448, 7.016409),13);
+}
+
+function displayangola(){
+	map.setView(new L.LatLng(-8.830395, 13.236284),14);	
+}
---
+++ Web src/js/template.js
@@ -0,0 +1,36 @@
+var map, featureList, boroughSearch = [], theaterSearch = [], museumSearch = [];
+
+$("#list-btn").click(function() {
+	animateSidebar();
+	return false;
+});
+
+$("#sidebar-toggle-btn").click(function() {
+	animateSidebar();
+	return false;
+});
+
+$("#sidebar-hide-btn").click(function() {
+	animateSidebar();
+	return false;
+});
+
+function animateSidebar() {
+	$("#sidebar").animate(
+		{
+			width: "toggle"
+		}, 350, function() {
+			map.invalidateSize();
+		}
+	);
+}
+
+map = new L.Map('map');
+
+function init() {
+	var osmUrl='http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
+	var osmAttrib='Map data © <a href="http://openstreetmap.org">OpenStreetMap</a> contributors';
+	var osm = new L.TileLayer(osmUrl, {minZoom: 1, maxZoom: 18, attribution: osmAttrib});
+	map.addLayer(osm);
+	map.setView([-1.746248, 11.930503],5);
+}
