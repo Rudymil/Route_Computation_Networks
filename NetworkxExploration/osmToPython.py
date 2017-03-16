573c3aa4496f3360a3f9721a01debdb02b730b89
Tofull <loic.messal@orange.fr>
2017-03-16 18:17:16 +0100

    clean file

--- NetworkxExploration/osmToPython.py
+++ NetworkxExploration/osmToPython.py
@@ -1,133 +1,190 @@
+"""
+Read directional graph from Open Street Maps osm format
 
-import OSMParser # OSMParser
-import matplotlib.pyplot as plt
-import networkx as netx
+Based on the osm to networkx tool from Tofull : https://gist.github.com/Tofull/49fbb9f3661e376d2fe08c2e9d64320e
+Use python3.6
+
+
+Copyright (C) 2017 Loïc Messal (github : Tofull)
+
+"""
+
+
+## Modules
+# Elementary modules
+import random
+
+# Data reader modules
+import OSMParser # file OSMParser.py
 import json
 
-import smopy
+
+# Networks modules
+import networkx as netx
+from networkx.readwrite import json_graph
+
+# Visualization modules
+import matplotlib.pyplot as plt
 import matplotlib as mpl
-mpl.rcParams['figure.dpi'] = mpl.rcParams['savefig.dpi'] = 200
+import smopy # osm map
+
+
+## CONFIGURATION VARIABLE
+DISPLAY = "OSM_MAP"
+# DISPLAY = "MAP"
+# DISPLAY = "GRAPH"
+
+
+def applyRandomWeight(graph):
+    for u,v,d in G.edges_iter(data=True):
+        G.add_weighted_edges_from([( u, v, random.randint(0,50000))], weight='weight_random')
+
+
+
 
 def savePath(filename, idList, graph):
+    """
+        export the computed route (as a list of followed points targeted by their id) as array of lat lon location
+
+        Result :
+        [[43.6026628, 3.8776791], [43.6026417, 3.877659], [43.602617, 3.8776495]]
+    """
     with open(filename, 'w') as f:
         print([[graph.node[p]['lat'], graph.node[p]['lon']] for p in idList], file=f)
 
 
-G=OSMParser.read_osm(OSMParser.download_osm(3.8748,43.5964,3.89,43.6072, cache=True))
-# G=OSMParser.read_osm(OSMParser.download_osm(-122.328,47.608,-122.31,47.61, cache=True))
-# print(G.node)
-# source_id = "3788313781"
-# target_id = "53195060"
+# Montpelllier Data
+G = OSMParser.read_osm(OSMParser.download_osm(3.8748,43.5964,3.89,43.6072, cache=True))
 source_id = "4445471419"
 target_id = "3945401293"
 
+# Initial Data
+# G = OSMParser.read_osm(OSMParser.download_osm(-122.328,47.608,-122.31,47.61, cache=True))
+# source_id = "3788313781"
+# target_id = "53195060"
+
+
 pos0 = (G.node[source_id]['lat'],G.node[source_id]['lon'])
 pos1 = (G.node[target_id]['lat'],G.node[target_id]['lon'])
-map = smopy.Map(pos0, pos1, z=15, margin=.1)
-plt.figure(figsize=(12,12));
-map.show_mpl(ax=plt);
 
-# plt.plot([G.node[n]['lat']for n in G], [G.node[n]['lon'] for n in G], 'o', color='k')
-# plt.plot([G.node[n]['lat']for n in G], [G.node[n]['lon'] for n in G], 'o', color='k')
-# x, y = map.to_pixels(G.node[:]['lon'], G.node[:]['lat'])
 
-x = []
-y = []
 
-for n in G:
-    a,b = map.to_pixels(G.node[n]['lat'], G.node[n]['lon'])
-    x.append(a)
-    y.append(b)
 
-plt.plot(x[:], y[:], 'o', color='k', ms=1);
-# print(G.node)
+## ROUTING COMPUTATION
+idList = []
+# [idList.append(p) for p in netx.shortest_path(G,source=source_id,target=target_id)] ## Uniform weight
+[idList.append(p) for p in netx.shortest_path(G,source=source_id,target=target_id, weight='length')] ## Weight based on length of ways
+# applyRandomWeight(G)
+# [idList.append(p) for p in netx.shortest_path(G,source=source_id,target=target_id, weight='weight_random')] ## Random weight
+
+
+
+
+## WITH osm map
+### Load map
+if (DISPLAY == "OSM_MAP"):
+    plt.figure(figsize=(12,12));
+    map = smopy.Map(pos0, pos1, z=15, margin=.1)
+    map.show_mpl(ax=plt);
+
+    ### Display each node
+    x = []
+    y = []
+    for n in G:
+        a,b = map.to_pixels(G.node[n]['lat'], G.node[n]['lon'])
+        x.append(a)
+        y.append(b)
+
+    plt.plot(x[:], y[:], 'o', color='k', ms=1);
+
+
+    ### Display step - nodes for the route
+    x = []
+    y = []
+    for n in idList:
+        a,b = map.to_pixels(G.node[n]['lat'], G.node[n]['lon'])
+        x.append(a)
+        y.append(b)
+
+    plt.plot(x[:], y[:], 'o' , color='b', ms=1);
+    plt.plot(x[:], y[:], color='b', ms=1);
+
+
+
+    ### Display source node
+    x = []
+    y = []
+    for n in [source_id]:
+        a,b = map.to_pixels(G.node[n]['lat'], G.node[n]['lon'])
+        x.append(a)
+        y.append(b)
+
+    plt.plot(x[:], y[:], 'o', color='g', ms=2);
+
+
+
+    ### Display target node
+    x = []
+    y = []
+    for n in [target_id]:
+        a,b = map.to_pixels(G.node[n]['lat'], G.node[n]['lon'])
+        x.append(a)
+        y.append(b)
+
+    plt.plot(x[:], y[:], 'o', color='r', ms=2);
+
+    plt.show()
+
+
+## WITHOUT osm map
+
+if (DISPLAY == "MAP"):
+    ## Display each node
+    plt.plot([G.node[n]['lat']for n in G], [G.node[n]['lon'] for n in G], 'o', color='k')
+
+    ## Display step - nodes for the route
+    plt.plot([G.node[n]['lat'] for n in idList], [G.node[n]['lon'] for n in idList], 'o' , color='b')
+    plt.plot([G.node[n]['lat'] for n in idList], [G.node[n]['lon'] for n in idList], color='b')
+
+    ## Display source node
+    plt.plot([G.node[source_id]['lat']], [G.node[source_id]['lon']], 'o', color='g')
+
+    ## Display target node
+    plt.plot([G.node[target_id]['lat']], [G.node[target_id]['lon']], 'o', color='r')
+
+    plt.show()
+
+if (DISPLAY == "GRAPH"):
+    #### DISPLAY GRAPH
+    netx.draw(G)
+    # plt.savefig("simple_path.png") # save as png
+    plt.show() # display
+
+
+### DATA PRINTER
+# print("nodes :", G.node)
 # print(G.node['3788313781']['data'])
 # print(G.node['3788313781']['data'].lat)
 # [print(G.node[n]['data'].lat) for n in G]
-
-import random
-
-
-# for u,v,d in G.edges_iter(data=True):
-#     G.add_weighted_edges_from([( u, v, random.randint(0,50000))], weight='weight_random')
-
-idList = []
-# [idList.append(p) for p in netx.shortest_path(G,source=source_id,target=target_id)]
-[idList.append(p) for p in netx.shortest_path(G,source=source_id,target=target_id, weight='length')]
-# [idList.append(p) for p in netx.shortest_path(G,source=source_id,target=target_id, weight='weight_random')]
-
-
+# print([[G.node[p]['lat'], G.node[p]['lon']] for p in idList])
 # print(G.nodes(data=True))
 # print(G.edges(data=True))
-
-# print([[G.node[p]['lat'], G.node[p]['lon']] for p in idList])
-# plt.plot([G.node[n]['lat'] for n in idList], [G.node[n]['lon'] for n in idList], 'o' , color='b')
-# plt.plot([G.node[n]['lat'] for n in idList], [G.node[n]['lon'] for n in idList], color='b')
-
-x = []
-y = []
-
-for n in idList:
-    a,b = map.to_pixels(G.node[n]['lat'], G.node[n]['lon'])
-    x.append(a)
-    y.append(b)
-
-plt.plot(x[:], y[:], 'o' , color='b', ms=1);
-plt.plot(x[:], y[:], color='b', ms=1);
-# print(x)
-# plt.plot([map.to_pixels(G.node[n]['lat'], G.node[n]['lon']) for n in idList], 'o' , color='b', ms=1)
-# plt.plot([map.to_pixels(G.node[n]['lat'], G.node[n]['lon']) for n in idList], color='b', ms=1)
-
-x = []
-y = []
-
-for n in [source_id]:
-    a,b = map.to_pixels(G.node[n]['lat'], G.node[n]['lon'])
-    x.append(a)
-    y.append(b)
-
-plt.plot(x[:], y[:], 'o', color='g', ms=2);
-
-x = []
-y = []
-
-for n in [target_id]:
-    a,b = map.to_pixels(G.node[n]['lat'], G.node[n]['lon'])
-    x.append(a)
-    y.append(b)
-
-plt.plot(x[:], y[:], 'o', color='r', ms=2);
-
-# plt.plot([G.node[source_id]['lat']], [G.node[source_id]['lon']], 'o', color='g')
-# plt.plot([G.node[target_id]['lat']], [G.node[target_id]['lon']], 'o', color='r')
-plt.show()
-
-
 # print([p for p in netx.all_shortest_paths(G,source="3788313781",target="53195060")])
 
-#
-# plt.plot([G.node[source_id]['lat']], [G.node[source_id]['lon']], 'o', color='g')
-# plt.plot([G.node[target_id]['lat']], [G.node[target_id]['lon']], 'o', color='r')
-# plt.show()
 
 
 
 
-#
-# netx.draw(G)
-# plt.draw()
-# plt.show()
-#
-# outdir="/home/roselia/Bureau/osmPython"
-# netx.write_shp(G, outdir)
-# netx.write_gml(G, "outdir.gml")
 
-from networkx.readwrite import json_graph
+# export graph
+## To gml
+# outfile="/tmp/export_graph.gml"
+# netx.write_gml(G, outfile)
+
+## To json
 data = json_graph.node_link_data(G)
 
-# s = json.dumps(data)
-s = json.dumps(data, indent=4)
+# s = json.dumps(data, indent=4)
 with open('JSONData.json', 'w') as f:
     json.dump(data, f, indent=4)
-
 savePath("itineraire.json", idList, G)
