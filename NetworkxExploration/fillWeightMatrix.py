d5d0b61003087068656254555c6ba47e48201cbc
Tofull <loic.messal@orange.fr>
2017-03-23 12:45:48 +0100

    refactor manager

--- NetworkxExploration/fillWeightMatrix.py
+++ NetworkxExploration/fillWeightMatrix.py
@@ -1,48 +1,49 @@
+## MODULES
 import json
+import networkx as netx
+from networkx.readwrite import json_graph
+import networkxGraphManager
 from shapely.geometry import shape, Point
 
-import matplotlib.pyplot as plt
-import networkx as netx
-import json
-from networkx.readwrite import json_graph
+## PARAMETERS
 
-def read_json_file(filename):
-    with open(filename) as f:
-        js_graph = json.load(f)
-    return json_graph.node_link_graph(js_graph)
+### Filenames
+graphFilenameInput = 'JSONData.json'
+unallowedAreaFilenameInput = 'border_polygons.json'
+graphFilenameOutput = 'test.json'
 
-def write_graph_to_json_file(filename, graph):
-    data = json_graph.node_link_data(G)
-
-    # s = json.dumps(data, indent=4)
-    with open(filename, 'w') as f:
-        json.dump(data, f, indent=4)
-
-filename = 'JSONData.json'
-G = read_json_file(filename)
-
-# load GeoJSON file containing sectors
-with open('border_polygons.json') as f:
-    js = json.load(f)
-
+### Default variables
 defaultWeight = 3.0
+weightKeyToChange = 'weight'
+
+## ALGORITHM
+
+# Load the graph from networkx export file
+G = networkxGraphManager.read_json_file(graphFilenameInput)
+
+# Load GeoJSON file containing sectors
+polygonList = []
+with open(unallowedAreaFilenameInput) as f:
+    js = json.load(f)
+for feature in js:
+    if feature["type"] == "Feature":
+        polygon = shape(feature['geometry'])
+        polygonList.append(polygon)
+
+# Set the default weight
 for u,v,d in G.edges_iter(data=True):
-    G.add_weighted_edges_from([( u, v, defaultWeight)], weight='weight')
+    G.add_weighted_edges_from([( u, v, defaultWeight)], weight=weightKeyToChange)
 
-
+# Set the weight from the unallowedAreaFilenameInput file
 for u,v,d in G.edges_iter(data=True):
     source = Point(G.node[u]["lat"], G.node[u]["lon"])
     target = Point(G.node[v]["lat"], G.node[v]["lon"])
 
-    for feature in js:
-        if feature["type"] == "Feature":
-            polygon = shape(feature['geometry'])
+    for polygon in polygonList:
             if polygon.contains(source) and polygon.contains(target):
                 if (feature["properties"]["weight"] > 0):
                     new_weight = d["weight"] + feature["properties"]["weight"]
                     G.add_weighted_edges_from([( u, v, new_weight)], weight='weight')
 
-
-
-# print("edge :", G.edges(data=True))
-write_graph_to_json_file("test.json", G)
+# Export the modified graph
+networkxGraphManager.write_graph_to_json_file(graphFilenameOutput, G)
