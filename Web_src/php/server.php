57a6cd92a17b4d92f86cf1a8986a27958106e92f
Romain MAZIERE <etude@rmaziere.fr>
2017-03-28 10:38:05 +0200

    Add bBox filter

--- Web_src/php/server.php
+++ Web_src/php/server.php
@@ -1,16 +1,19 @@
 <?php
 include_once("./queryMaker.inc.php");
 
-if (isset($_GET["type"]) && ($_GET["type"] == "warning_zone" || $_GET["type"] == "anomaly_zone")) {
+if (isset($_GET["type"]) && ($_GET["type"] == "warning_zone" ||
+                              $_GET["type"] == "anomaly_zone" ||
+                              $_GET["type"] == "warning_type" ||
+                              $_GET["type"] == "anomaly_type")) {
   if ($_GET["type"] == "warning_zone") {
-    $zones_list = "SELECT z.id, ST_AsGeoJSON(z.geom) AS geojson, t.name, risk_intensity AS intensity FROM warning_zone z, risk t WHERE z.risk_type = t.id ";
+    $zones_list = "SELECT z.id, ST_AsGeoJSON(z.geom) AS geojson, t.name, risk_intensity AS intensity FROM warning_zone z, risk t WHERE z.risk_type = t.id;";
   }elseif ($_GET["type"] == "anomaly_zone") {
-    $zones_list = "SELECT z.id, ST_AsGeoJSON(z.geom) AS geojson, t.name, description FROM anomaly_zone z, anomaly t WHERE z.anomaly_type = t.id ";
+    $zones_list = "SELECT z.id, ST_AsGeoJSON(z.geom) AS geojson, t.name, description FROM anomaly_zone z, anomaly t WHERE z.anomaly_type = t.id;";
+  }elseif ($_GET["type"] == "warning_type") {
+    $zones_list = "SELECT id, name FROM risk ORDER BY name;";
+  }elseif ($_GET["type"] == "anomaly_type") {
+    $zones_list = "SELECT id, name FROM anomaly ORDER BY name;";
   }
-
-  //Add the LIMIT
-  //$zones_list .= " LIMIT 1";
-  $zones_list .= ";";
 
   if (isset($_GET["DEBUG"])) {
     header('Content-type: text/html; charset=utf-8');
