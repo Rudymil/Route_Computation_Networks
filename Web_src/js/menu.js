2e9a157ac44bf012e4e7c093747f34939fe236f4
MILLET <rmillet8@sfr.fr>
2017-03-31 14:37:09 +0200

    DEBUG

--- Web_src/js/menu.js
+++ Web_src/js/menu.js
@@ -655,7 +655,7 @@ function send_ajax_geojson(geojson,type,url){ // envoie en ajax le geojson et le
 	$.ajax({
 		url : url,
 		type : 'POST',
-		data : type+'='+geojson,
+		data : /*type+'='+*/geojson,
 		dataType : 'json',
 		success : function(code, statut){
 			if (DEBUG){
