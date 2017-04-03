257ac65b98e929cfa44ec23897b27e25a27c11d4
MILLET <rmillet8@sfr.fr>
2017-04-03 14:58:27 +0200

    selection_area_DOM

--- Web_src/js/selection_of_the_area.js
+++ Web_src/js/selection_of_the_area.js
@@ -1,4 +1,4 @@
-$("body").ready(function(){ // lorsque le body est charge
+function countries_DOM(){ // construit les boutons
 	if (DEBUG){
 		console.log("EVENT : $('body').ready");
 		console.log("EVENT : $('body').ready json_countries : ", json_countries);
@@ -6,7 +6,7 @@ $("body").ready(function(){ // lorsque le body est charge
 	for (object in json_countries){
 		$("#panel-element-204612>.panel-body").append("div").addClass("row").append("div").addClass("col-xs-12").append("center").append("button").addType("button").addClass("btn btn-primary btn-xm").addId(json_countries[object]["name"]).append(json_countries[object]["name"]);
 	}
-});
+}
 
 $("#nigeria").click(function(){ // reset sur port harcourt
 	map.setView(new L.LatLng(4.804448, 7.016409),13);
