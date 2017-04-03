cafebc7a3d4ac35cdc8bfecce8cc1fcda011bd60
MILLET <rmillet8@sfr.fr>
2017-04-03 16:27:54 +0200

    selection_area_DOM

--- Web_src/js/selection_of_the_area.js
+++
@@ -1,7 +0,0 @@
-$("#Nigeria").click(function(){ // reset sur port harcourt
-	map.setView(new L.LatLng(4.804448, 7.016409),13);
-});
-
-$("#Angola").click(function(){ // reset sur luanda
-	map.setView(new L.LatLng(-8.830395, 13.236284),14);
-});
