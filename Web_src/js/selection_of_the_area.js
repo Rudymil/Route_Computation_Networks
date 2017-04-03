75e420bd822212d318bb6ab3a98fa3c7508f6068
MILLET <rmillet8@sfr.fr>
2017-04-03 15:21:00 +0200

    selection_area_DOM

--- Web_src/js/selection_of_the_area.js
+++ Web_src/js/selection_of_the_area.js
@@ -8,15 +8,15 @@ function countries_DOM(){
 				console.log("countries_DOM object : ", object);
 				console.log("countries_DOM json_countries[object] : ", json_countries[object]);
 			}
-			$("#panel-element-204612>.panel-body").append("div").addClass("row").append("div").addClass("col-xs-12").append("center").append("button").addType("button").addClass("btn btn-primary btn-xm").addId(json_countries[object]["name"]).append(json_countries[object]["name"]);
+			$("#panel-element-204612>.panel-body").append("div").addClass("row").append("div").addClass("col-xs-12").append("center").append("button").addClass("btn btn-primary btn-xm").append(json_countries[object]["name"]);
 		}
 	}
 }
 
-/*$("#nigeria").click(function(){ // reset sur port harcourt
+$("#nigeria").click(function(){ // reset sur port harcourt
 	map.setView(new L.LatLng(4.804448, 7.016409),13);
 });
 
 $("#angola").click(function(){ // reset sur luanda
 	map.setView(new L.LatLng(-8.830395, 13.236284),14);
-});*/
+});
