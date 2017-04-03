9bc4f66c42a459f24b247c3eb6466a5d021f39b8
MILLET <rmillet8@sfr.fr>
2017-04-03 17:20:22 +0200

    liste_countries

--- Web_src/js/menu.js
+++ Web_src/js/menu.js
@@ -152,11 +152,11 @@ function ajax_countries(url){ // requete ajax sur les pays
 								console.log("ajax_countries liste_countries[json_countries[object]['name']][0] : ", liste_countries[json_countries[object]['name']][0]);
 								console.log("ajax_countries liste_countries[json_countries[object]['name']][1] : ", liste_countries[json_countries[object]['name']][1]);
 							}
-							$("#"+json_countries[object]['name']).click(function(object){ // reset view
+							$("#"+json_countries[object]['name']).click(function(this){ // reset view
 								if (DEBUG){
+									console.log(this);
 									console.log("ajax_countries liste_countries[json_countries[object]['name']][0] : ", liste_countries[json_countries[object]['name']][0]);
 									console.log("ajax_countries liste_countries[json_countries[object]['name']][1] : ", liste_countries[json_countries[object]['name']][1]);
-									console.log(object);
 								}
 								map.setView(liste_countries[json_countries[object]['name']][0],liste_countries[json_countries[object]['name']][1]);
 							});
