7c051e186d5e991bd56a01cba30a6174d79988f1
MILLET <rmillet8@sfr.fr>
2017-04-03 14:51:03 +0200

    selection_area_DOM

--- Web_src/js/selection_of_the_area.js
+++ Web_src/js/selection_of_the_area.js
@@ -1,6 +1,7 @@
 $("body").ready(function(){ // lorsque le body est charge
 	if (DEBUG){
 		console.log("EVENT : $('body').ready");
+		console.log("EVENT : $('body').ready json_countries : ", json_countries);
 	}
 	for (object in json_countries){
 		$("#panel-element-204612>.panel-body").append("div").addClass("row").append("div").addClass("col-xs-12").append("center").append("button").addType("button").addClass("btn btn-primary btn-xm").addId(json_countries[object]["name"]).append(json_countries[object]["name"]);
