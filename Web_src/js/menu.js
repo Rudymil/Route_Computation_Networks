a3e1d95cd687a3a69bc585b5c2847d03176d4626
MILLET <rmillet8@sfr.fr>
2017-04-03 16:05:00 +0200

    selection_area_DOM

--- Web_src/js/menu.js
+++ Web_src/js/menu.js
@@ -145,7 +145,7 @@ function ajax_countries(url){ // requete ajax sur les pays
 								console.log("ajax_countries object : ", object);
 								console.log("ajax_countries json_countries[object] : ", json_countries[object]);
 							}
-							$("#panel-element-204612>.panel-body").append("<div class='row'><div class='col-xs-12' ><center><button type='button' class='btn btn-primary btn-xm' ></button></center></div></div>");
+							$("#panel-element-204612>.panel-body").append("<div class='row'><div class='col-xs-12' ><center><button type='button' class='btn btn-primary btn-xm' id="ajax_countries json_countries[object]['name']">"ajax_countries json_countries[object]['name']"</button></center></div></div>");
 						}
 					}
 				}
