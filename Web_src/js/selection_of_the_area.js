c2b3b3c59f7ef6eebc738f7e4b77824206bdf822
MILLET <rmillet8@sfr.fr>
2017-04-03 15:44:58 +0200

    selection_area_DOM

--- Web_src/js/selection_of_the_area.js
+++ Web_src/js/selection_of_the_area.js
@@ -1,18 +1,3 @@
-function countries_DOM(){
-	if (DEBUG){
-		console.log("countries_DOM json_countries : ", json_countries);
-	}
-	if (json_countries.length > 0){ // si y a des pays
-		for (object in json_countries){
-			if (DEBUG){
-				console.log("countries_DOM object : ", object);
-				console.log("countries_DOM json_countries[object] : ", json_countries[object]);
-			}
-			$("#panel-element-204612>.panel-body").append('<div class="row"><div class="col-xs-12" ></div><center><button type="button" class="btn btn-primary btn-xm" id='json_countries[object]["name"]'>'json_countries[object]["name"]'</button></center></div>');
-		}
-	}
-}
-
 $("#nigeria").click(function(){ // reset sur port harcourt
 	map.setView(new L.LatLng(4.804448, 7.016409),13);
 });
