a4ae391ad7b67a360c32ef8f540e483cf05c9ad4
MILLET <rmillet8@sfr.fr>
2017-04-04 11:52:47 +0200

    new_geo_JSON

--- Web_src/js/menu.js
+++ Web_src/js/menu.js
@@ -109,7 +109,7 @@ function ajax_countries(url) { // requete ajax sur les pays
                                 console.log("ajax_countries object : ", object);
                                 console.log("ajax_countries json_countries[object] : ", json_countries[object]);
                             }
-                            $("#panel-element-204612>.panel-body").append("<div class='row'><div class='col-xs-12' ><center><button type='button' class='btn btn-primary btn-xm' style='margin-bottom:5px;' id=" + json_countries[object]['properties']['name'] + ">" + json_countries[object]['properties']['name'] + "</button></center></div></div>"); // ajout du bouton
+                            $("#panel-element-204612>.panel-body").append("<div class='row'><div class='col-xs-12' ><center><button type='button' class='btn btn-primary btn-xm' style='margin-bottom:5px;' id=" + object + ">" + json_countries[object]['properties']['name'] + "</button></center></div></div>"); // ajout du bouton
                             $("#" + object).click(function(event) { // reset view
                                 if (DEBUG) {
                                     console.log("event.target.id : ", event.target.id);
