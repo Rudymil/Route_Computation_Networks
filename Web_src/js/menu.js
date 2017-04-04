4d28771b41af92156d731e404f0bafdab0822c2c
MILLET <rmillet8@sfr.fr>
2017-04-04 11:44:13 +0200

    new_geo_JSON

--- Web_src/js/menu.js
+++ Web_src/js/menu.js
@@ -99,10 +99,9 @@ function ajax_countries(url) { // requete ajax sur les pays
                 var json = resultat.responseJSON;
                 if (!$.isEmptyObject(json)) { // si le resultat json n est pas vide
                     if (DEBUG) {
-                        console.log("json : ", json);
+                        console.log("json['features'] : ", json['features']);
                     }
-                    json_countries = json;
-                    //countries_DOM(); // construit les boutons
+                    json_countries = json['features'];
                     if (json_countries.length > 0) { // si y a des pays
                         var fonctions = new Array();
                         for (object in json_countries) {
@@ -110,18 +109,12 @@ function ajax_countries(url) { // requete ajax sur les pays
                                 console.log("ajax_countries object : ", object);
                                 console.log("ajax_countries json_countries[object] : ", json_countries[object]);
                             }
-                            $("#panel-element-204612>.panel-body").append("<div class='row'><div class='col-xs-12' ><center><button type='button' class='btn btn-primary btn-xm' style='margin-bottom:5px;' id=" + json_countries[object]['name'] + ">" + json_countries[object]['name'] + "</button></center></div></div>"); // ajout du bouton
-                            if (DEBUG) {
-                                console.log("ajax_countries liste_countries[json_countries[object]['name']][0] : ", liste_countries[json_countries[object]['name']][0]);
-                                console.log("ajax_countries liste_countries[json_countries[object]['name']][1] : ", liste_countries[json_countries[object]['name']][1]);
-                            }
-                            $("#" + json_countries[object]['name']).click(function(event) { // reset view
+                            $("#panel-element-204612>.panel-body").append("<div class='row'><div class='col-xs-12' ><center><button type='button' class='btn btn-primary btn-xm' style='margin-bottom:5px;' id=" + json_countries[object]['properties']['name'] + ">" + json_countries[object]['properties']['name'] + "</button></center></div></div>"); // ajout du bouton
+                            $("#" + json_countries[object]['properties']['name']).click(function() { // reset view
                                 if (DEBUG) {
-                                    console.log("$(this).id : ", event.target.id);
-                                    console.log("ajax_countries liste_countries[json_countries[object]['name']][0] : ", liste_countries[json_countries[object]['name']][0]);
-                                    console.log("ajax_countries liste_countries[json_countries[object]['name']][1] : ", liste_countries[json_countries[object]['name']][1]);
+                                    console.log("ajax_countries json_countries[object]['geometry']['coordinates'] : ", json_countries[object]['geometry']['coordinates']);
                                 }
-                                map.setView(liste_countries[event.target.id][0], liste_countries[event.target.id][1]);
+                                map.setView(json_countries[object]['geometry']['coordinates'], zoom);
                             });
                         }
                     }
