36ef190a157f810eb1cacc6e9b5262813f7cc6b2
MILLET <rmillet8@sfr.fr>
2017-04-05 16:19:10 +0200

    notify

--- Web_src/js/menu.js
+++ Web_src/js/menu.js
@@ -943,7 +943,7 @@ function send_ajax_point(point) {
                 }
                 $.notify({
                     title: "<strong>Point request</strong>",
-                    message: statut
+                    message: "Out of the area"
                 }, {
                     type: "danger",
                     placement: {
@@ -975,10 +975,7 @@ function send_ajax_point(point) {
                                 countrystart[1] = reponse["name"];
                                 if (countryend[0] != null || countryend[0] != undefined && countryend[1] != null || countryend[0] != undefined) { // si point d arrivee
                                     if (countrystart[0] == countryend[0] && countrystart[1] == countryend[1]) { // si egalite
-
-										controlPenalty.spliceWaypoints(0, 1, position);
-
-
+                                        controlPenalty.spliceWaypoints(0, 1, position);
                                     } else { // sinon
                                         $.notify({
                                             title: "<strong>Localisation</strong>",
@@ -992,8 +989,8 @@ function send_ajax_point(point) {
                                         });
                                     }
                                 } else {
-									controlPenalty.spliceWaypoints(0, 1, position);
-								}
+                                    controlPenalty.spliceWaypoints(0, 1, position);
+                                }
 
                             }
                             if (point[2] == "end") { // si point d arrivee
@@ -1007,9 +1004,7 @@ function send_ajax_point(point) {
                                 countryend[1] = reponse["name"];
                                 if (countrystart[0] != null || countrystart[0] != undefined && countrystart[1] != null || countrystart[0] != undefined) { // si point de depart
                                     if (countrystart[0] == countryend[0] && countrystart[1] == countryend[1]) { // si egalite
-
-										controlPenalty.spliceWaypoints(controlPenalty.getWaypoints().length - 1, 1, position);
-
+                                        controlPenalty.spliceWaypoints(controlPenalty.getWaypoints().length - 1, 1, position);
                                     } else { // sinon
                                         $.notify({
                                             title: "<strong>Localisation</strong>",
@@ -1023,9 +1018,8 @@ function send_ajax_point(point) {
                                         });
                                     }
                                 } else {
-									controlPenalty.spliceWaypoints(controlPenalty.getWaypoints().length - 1, 1, position);
-								}
-
+                                    controlPenalty.spliceWaypoints(controlPenalty.getWaypoints().length - 1, 1, position);
+                                }
                             }
                             if (point[2] == "step") { // si point intermediaire
                                 if (DEBUG) {
