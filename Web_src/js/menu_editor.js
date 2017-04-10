971f92ac30c1b8196cd9f803b781ac438837d05d
MILLET <rmillet8@sfr.fr>
2017-04-10 11:19:46 +0200

    validated

--- Web_src/js/menu_editor.js
+++ Web_src/js/menu_editor.js
@@ -297,7 +297,7 @@ function add_warning_zones(url, bbox) {
     $.ajax({
         url: url,
         type: 'GET',
-        data: 'type=' + string_warning_zone + '&bbox=' + bbox,
+        data: 'type=' + string_warning_zone + '&bbox=' + bbox + '&validated=true',
         dataType: 'json',
         success: function(code_json, statut) {
             if (DEBUG) {
@@ -346,16 +346,6 @@ function add_warning_zones(url, bbox) {
                     if (DEBUG) {
                         console.log("add_warning_zones json :", json);
                     }
-                    if (warning_zones.length > 0) {
-                        for (element in warning_zones) { // pour chaque warning zones
-                            if (DEBUG) {
-                                console.log("element :", element);
-                                console.log("warning_zones[element] :", warning_zones[element]);
-                            }
-                            warning_zones[element].removeFrom(map); // on enleve les warning zones de la map
-                        }
-                    }
-                    warning_zones = new Array(); // on vide les warning zones
                     if (json["features"].length > 0) {
                         for (element in json["features"]) { // pour chaque object du geojson
                             if (DEBUG) {
@@ -394,13 +384,116 @@ function add_warning_zones(url, bbox) {
                             var divLegend = "";
                             // loop through our density intervals and generate a label with a colored square for each interval
                             for (var i = 0; i < grades.length; i++) {
-                                divLegend += ('<i style="background:' + getColor(grades[i] + 1) + '"></i> ' +
-                                    grades[i] + (grades[i + 1] ? '&ndash;' + grades[i + 1] + '<br>' : '+'))
+                                divLegend += ('<i style="background:' + getColor(grades[i] + 1) + '"></i> ' + grades[i] + (grades[i + 1] ? '&ndash;' + grades[i + 1] + '<br>' : '+'));
                             }
                             $(div).html(divLegend);
                             return div;
                         };
                         legend.addTo(map);
+                        /*$.notify(
+                            {
+                                title: "<strong>Warning zones request</strong>",
+                                message: 'received'
+                            },{
+                                type: "success",
+                                placement: {
+                                    from: "bottom",
+                                    align: "center"
+                                }
+                            }
+                        );*/
+                    } else {
+                        //notify_warning_zones_none();
+                    }
+                } else {
+                    if (DEBUG) {
+                        console.log("add_warning_zones json :", json);
+                    }
+                    if (warning_zones.length > 0) {
+                        remove_warning_zones();
+                    }
+                    //notify_warning_zones_none();
+                }
+            }
+        }
+    });
+    $.ajax({
+        url: url,
+        type: 'GET',
+        data: 'type=' + string_warning_zone + '&bbox=' + bbox + '&validated=false',
+        dataType: 'json',
+        success: function(code_json, statut) {
+            if (DEBUG) {
+                console.log("add_warning_zones code_json : ", code_json);
+                console.log("add_warning_zones statut : ", statut);
+            }
+            /*$.notify(
+                {
+                    title: "<strong>Warning zones request</strong>",
+                    message: statut
+                },{
+                    type: "success",
+                    placement: {
+                        from: "bottom",
+                        align: "center"
+                    }
+                }
+            );*/
+        },
+        error: function(resultat, statut, erreur) {
+            if (DEBUG) {
+                console.log("add_warning_zones resultat : ", resultat);
+                console.log("add_warning_zones statut : ", statut);
+                console.log("add_warning_zones erreur : ", erreur);
+            }
+            /*$.notify(
+                {
+                    title: "<strong>Warning zones request</strong>",
+                    message: statut
+                },{
+                    type: "danger",
+                    placement: {
+                        from: "bottom",
+                        align: "center"
+                    }
+                }
+            );*/
+        },
+        complete: function(resultat, statut) {
+            if (resultat.status == '200') {
+                if (DEBUG) {
+                    console.log("add_warning_zones resultat.status :", resultat.status);
+                }
+                var json = resultat.responseJSON;
+                if (!$.isEmptyObject(json) && json != undefined) { // si le resultat json n est pas vide
+                    if (DEBUG) {
+                        console.log("add_warning_zones json :", json);
+                    }
+                    if (json["features"].length > 0) {
+                        for (element in json["features"]) { // pour chaque object du geojson
+                            if (DEBUG) {
+                                console.log("add_warning_zones element :", element);
+                                console.log("add_warning_zones json['features'][element] :", json["features"][element]);
+                                console.log("add_warning_zones json['features'][element]['properties'].intensity :", json["features"][element]["properties"].intensity);
+                            }
+                            var shape = L.geoJSON(json["features"][element]).getLayers()[0];
+                            var colorZone = getColor(json["features"][element]["properties"].intensity);
+                            shape.setStyle({ // transforme en layer et change le style
+                                fillColor: colorZone,
+                                color: 'red'
+                            });
+                            shape.bindPopup(getPopupContentmenu(json["features"][element]));
+                            featureLayerw.addLayer(shape);
+                            warning_zones.push(shape); // remplir la warning zone
+                        }
+                        layer_group_warning_zones = L.layerGroup(warning_zones); // groupe des couches warning zones
+                        overlayMaps["Warning zones"] = layer_group_warning_zones; // menu
+                        if (Lcontrollayers != undefined || Lcontrollayers != null) {
+                            Lcontrollayers.remove();
+                        }
+                        Lcontrollayers = L.control.layers(null, overlayMaps, {
+                            position: 'topleft'
+                        }).addTo(map); // ne pas oublier le null
                         /*$.notify(
                             {
                                 title: "<strong>Warning zones request</strong>",
